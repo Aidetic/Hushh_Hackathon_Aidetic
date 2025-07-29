5cf704291089789d567e546f27f00252fd1ea888
root <root@e2e-63-168.ssdcloudindia.net>
2025-07-29 14:59:36 +0530

    fixed json file import

--- personal_calendar_agents/core/settings.py
+++ personal_calendar_agents/core/settings.py
@@ -1,5 +1,8 @@
 import os
 from dotenv import load_dotenv
+from pathlib import Path
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 class Settings:
 
@@ -7,7 +10,7 @@ class Settings:
         load_dotenv()
         self.GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
         self.GEMINI_MODEL_NAME = os.getenv("GEMINI_MODEL_NAME", "")
-        self.credentials_json = os.getenv('GOOGLE_CREDENTIALS_JSON')
+        self.credentials_json = os.path.join(BASE_DIR, os.getenv('GOOGLE_CREDENTIALS_JSON'))
         self.credentials_path = os.getenv('GOOGLE_CREDENTIALS_PATH')
 
 
